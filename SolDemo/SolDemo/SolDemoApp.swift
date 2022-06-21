f988e4f1834b38bf105c930737c6c05c5a897692
Dan Schnabel <dans@clearbridgemobile.com>
2022-06-21 11:26:28 -0400

    Initialize with local package

---
+++ SolDemo/SolDemo/SolDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolDemoApp.swift
+//  SolDemo
+//
+//  Created by Danny Schnabel on 2022-06-21.
+//
+
+import SwiftUI
+
+@main
+struct SolDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
